ca3e765767d95dfca850c307c7a15b0ff63d291b
antoniobutkovic <antonio.butkoviic@gmail.com>
2018-04-08 16:33:37 +0200

    Add two tasks

---
+++ src/Task1.java
@@ -0,0 +1,22 @@
+import javax.swing.*;
+
+public class Task1 {
+
+//    1. Write a program that asks the user for a number n and prints the sum of the numbers 1 to n
+
+    public static void main(String[] args) {
+
+        int n;
+        int sum = 0;
+
+        do {
+            n = Integer.parseInt(JOptionPane.showInputDialog(null, "Unesi n:"));
+        }while (n < 1);
+
+        for (int i = 1; i <= n; i++) {
+            sum += i;
+        }
+
+        System.out.print(sum);
+    }
+}
---
+++ src/Task2.java
@@ -0,0 +1,37 @@
+import javax.swing.*;
+
+public class Task2 {
+
+//  2. Write a program that asks the user for a number n and gives them the possibility to choose between computing the sum and computing the product of 1,…,n. pr
+
+    public static void main(String[] args) {
+
+        int n;
+        int result;
+        String operation;
+
+        do {
+            n = Integer.valueOf(JOptionPane.showInputDialog(null, "Unesi n:"));
+        }while (n < 1);
+
+        do {
+            operation = JOptionPane.showInputDialog(null, "Izaberi radnju s/p");
+        }while (!operation.equals("s") && !operation.equals("p"));
+
+        if (operation.equals("s")){
+            result = 0;
+            for (int i = 1; i <= n; i++) {
+                result += i;
+            }
+            System.out.println(result);
+        }else if (operation.equals("p")){
+            result = 1;
+            for (int i = 1; i <= n; i++) {
+                result *= i;
+            }
+            System.out.println(result);
+        }
+
+
+    }
+}
