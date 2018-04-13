f29867f08a06fe14967378c9244a30cc8356ff81
antoniobutkovic <antonio.butkoviic@gmail.com>
2018-04-13 18:28:40 +0200

    Fix Task9

--- src/butkovic/Task9.java
+++ src/butkovic/Task9.java
@@ -1,18 +1,23 @@
 package butkovic;
 
+import java.util.Random;
+
 public class Task9 {
 
 //    9. Write a program that prints Matrix code lookalike in console.
 
     public static void main(String[] args) {
-        
-        int matrix [][] = {{13, 99, 56},{12, 68, 32}, {87, 20, 33}};
 
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                System.out.print(matrix[i][j] + " ");
+        Random random = new Random();
+        int counter  = 0;
+
+        for (;;){
+            System.out.print(random.nextInt(2));
+            counter++;
+            if (counter == 50){
+                System.out.println();
+                counter = 0;
             }
-            System.out.println();
         }
     }
 
